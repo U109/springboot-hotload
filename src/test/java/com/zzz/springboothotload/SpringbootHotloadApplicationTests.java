aa4bddd1a869bb3c026464319fbfbb640a6ec5de
zhang-zhongzhen <1098598203@qq.com>
2024-01-02 18:05:30 +0800

    init

---
+++ src/test/java/com/zzz/springboothotload/SpringbootHotloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzz.springboothotload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHotloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
