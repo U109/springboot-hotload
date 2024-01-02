b55597067142c84064b11ea8d54c8947643d58f2
zhang-zhongzhen <1098598203@qq.com>
2024-01-02 18:24:46 +0800

    init

---
+++ src/main/java/com/zzz/springboothotload/SpringBootHotLoadApplication.java
@@ -0,0 +1,16 @@
+package com.zzz.springboothotload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 张忠振
+ */
+@SpringBootApplication
+public class SpringBootHotLoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHotLoadApplication.class, args);
+    }
+
+}
